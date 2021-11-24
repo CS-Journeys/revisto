94e6a6df90fd38d239d038bbed26f885830ed972
daniel-luper <jdaniel.luper@gmail.com>
2021-11-24 17:13:47 -0600

    remove unnecessary log

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,5 @@ import axios from 'axios'
 import App from './App';
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
-console.log(process.env.BASE_URL);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
