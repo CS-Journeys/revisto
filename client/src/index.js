06e8afd6f7cf50f36b1e32a4827da375c9b707f8
daniel-luper <jdaniel.luper@gmail.com>
2021-11-24 16:48:26 -0600

    Server Config

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
 
 import App from './App';
+
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
+console.log(process.env.BASE_URL);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/posts', postRoutes)
+app.use('/api/posts', postRoutes)
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
