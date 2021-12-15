7907bb86241a3e5152c04719432557d3397a7c16
thereallucyray <lucy@therayfamily.org>
2021-12-15 15:52:47 -0600

    Added Roboto body font and updated logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../logo-tmp.png'
+import logo from '../revistoLogo.svg'
 
 class Navbar extends Component {
 
     render() {
         return (
         <nav class="navbar navbar-expand-sm nav-bg">
-            <img src={logo} alt="Logo" className="nav-logo" id="logo"/>
+            <img src={logo} alt="Logo" className="img-fluid nav-logo" id="logo"/>
             <ul class="navbar-nav">
                 <li class="nav-item">
                 <a class="nav-link" href="#">Home</a>
