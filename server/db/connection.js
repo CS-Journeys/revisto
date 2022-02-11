ac90d964ac5ef7314c3a7b558c32eb8b66628903
daniel-luper <jdaniel.luper@gmail.com>
2022-02-11 08:25:07 -0600

    replace console.log with logger.info/error

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,24 +1,25 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import logger from "../utils/logger.js";
 
 // Load .env configuration
 dotenv.config();
 if (!("ATLAS_URI" in process.env)) {
-  console.error("MongoDB connection URL not defined. Ask the lead developers for more info.");
+  logger.error("MongoDB connection URL not defined. Ask the lead developers for more info.");
   throw new Error("Missing 'ATLAS_URI' parameter in .env");
 }
 
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) {
-    console.error(err);
+    logger.error(err);
     throw new Error("MongoDB connection error");
   } else {
-    console.log("Connected to MongoDB");
+    logger.info("Connected to MongoDB");
   }
 });
 
 let db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Mongoose Connection Error: '));
+db.on('error', (err) => {logger.error("Mongoose Connection Error: " + err)});
 
 export default db;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+import logger from "./utils/logger.js";
 import db from "./db/connection.js";
 import app from "./app.js";
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
