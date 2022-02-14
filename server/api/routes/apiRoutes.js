7a28146126e313dc989d5459286706db49c253ea
daniel-luper <jdaniel.luper@gmail.com>
2022-02-13 19:50:35 -0600

    permissions fix

--- server/api/routes/apiRoutes.js
+++ server/api/routes/apiRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import { authenticate } from "../../core/utils/jwtAuth.js";
 import getUserObject from "../middleware/getUserObject.js";
+import getRoute from "../../core/middleware/getRoute.js";
 import checkPermissions from "../middleware/permissions.js";
 
 import postRoutes from "./postsRoutes.js";
@@ -10,6 +11,7 @@ import userRoutes from "./usersRoutes.js";
 const router = express.Router();
 
 router.use(authenticate);
+router.use(getRoute);
 router.use(getUserObject);
 router.use(checkPermissions);
 router.use("/posts", postRoutes);
