94a249108ac0466b634bdd443834ead7a1e1b109
daniel-luper <jdaniel.luper@gmail.com>
2022-03-01 12:06:12 -0600

    remove tests

--- server/jest.config.mjs
+++
@@ -1,8 +0,0 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
-export default {
-  setupFiles: ["dotenv/config"]
-};
