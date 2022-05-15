a0ba1dbda9de14924346803230469cc631daa7a2
daniel-luper <jdaniel.luper@gmail.com>
2022-05-15 11:06:46 -0500

    authorization

--- server/services/admin/routes/adminRoutes.js
+++ server/services/admin/routes/adminRoutes.js
@@ -5,9 +5,11 @@ import postsRoutes from "./postsRoutes.js";
 import reportsRoutes from "./reportsRoutes.js";
 import emailRoutes from "./emailRoutes.js";
 import messagesRoutes from "./messagesRoutes.js";
+import authorize from "../middleware/authorize.js";
 
 const router = express.Router();
 
+router.use(authorize);
 router.use("/users", usersRoutes);
 router.use("/reports", reportsRoutes);
 router.use("/posts", postsRoutes);
