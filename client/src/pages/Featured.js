9d85573ea6a257fd82ff339d715d99a96e475575
lukalelovic <lukalelovic@gmail.com>
2022-05-16 14:12:53 -0500

    center end text

--- client/src/pages/Featured.js
+++ client/src/pages/Featured.js
@@ -2,14 +2,14 @@ import React from "react";
 import PostsList from "../components/post/PostsList";
 import { usePosts } from "../hooks/postHook";
 
-const Featured = (props) => {
+const Featured = () => {
     const attrib = "reactionCount";
     const posts = usePosts({ sortAttribute : attrib });
 
     return (
         <div>
             <div className="container-fluid">
-                <h1 className="text-center">Featured</h1>
+                <h1 className="display-4 text-center">Featured</h1>
                 <PostsList posts={posts} />
             </div>
         </div>
