fd268984c3eb74ed46c20b625ed5797e7c3e8b9b
daniel-luper <jdaniel.luper@gmail.com>
2022-07-06 00:21:33 -0400

    updated about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -12,45 +12,67 @@ const About = () => {
     return (
         <article className="about-cover">
             <div className="container-fl">
-                <h1 className="text-center display-4">What is Revisto?</h1>
-                <p className="about-lead">
-                    Revisto is a public, anonymous journaling website.
-                </p>
-
+                <h1 className="text-center display-4">About</h1>
+                <h2>What is Revisto?</h2>
                 <p className="about-p">
-                    Our goal is for anyone to be able to post anything, anytime,
-                    about their lives from around the world! We want to create a
-                    social media app that does not use addictive tactics or feel
-                    like a popularity contest!
+                    Revisto is a journaling website. We designed Revisto to be a
+                    healthy space to share your thoughts and explore others'
+                    internal worlds as well.
+                </p>
+                <br />
+            </div>
+            <div>
+                <h2 className="">Core Features</h2>
+                <p className="about-p">
+                    <b> - PUBLIC:</b> We want anyone to be able to share
+                    anything, anytime, about their lives from around the world!
                 </p>
                 <p className="about-p">
-                    The site is currently being developed and maintained as part
-                    of an ongoing project by CS Journeys.
+                    <b> - ANONYMOUS:</b> To facilitate honesty, all posts are
+                    anonymous.
+                </p>
+                <p className="about-p">
+                    <b> - QUALITATIVE:</b> Human value is not a number. We don't
+                    want users to fixate on maximizing likes. We want you to
+                    know that at least one person read your post.
+                </p>
+                <p className="about-p">
+                    <b> - TEXT-ONLY:</b> Text is easy to moderate, and text
+                    allows users to focus on the raw thoughts which transcend
+                    superficial boundaries.
                 </p>
                 <br />
             </div>
 
             <div>
-                <h2 className="">Motivation</h2>
+                <h2>The Team</h2>
                 <p className="about-p">
-                    CS Journeys is a student group dedicated to taking on new
-                    projects and developing working applications. We love what
-                    we do and want to share our progress with the world!
+                    CS Journeys is a student programming group dedicated to
+                    learning, taking on new projects, and developing working
+                    applications. We love what we do and want to share our
+                    progress with the world!
                 </p>
-            </div>
-
-            <div>
                 <div className="">
                     <img src={teamImage} className="w-25" alt="The Team" />
                 </div>
-                <br/>
+                <br />
             </div>
+
             <div>
-                <h2 className="">Contact Us</h2>
+                <h2>Project State</h2>
                 <p className="about-p">
-                    We would appreciate any and all feedback you have about the
-                    site! Email us at revisto.live@gmail.com
+                    While we welcome users to this experimental social media
+                    platform, we recognize that it could be even more. If you
+                    are interested in scaling up Revisto or building something
+                    similar, please reach out to us at revisto.live@gmail.com.
+                    Any other questions or feedback would also be highly
+                    appreciated!
                 </p>
+                <p className="about-p">
+                    At the very least, we hope that Revisto will inspire you to
+                    re-imagine what social media can be :)
+                </p>
+                <br />
             </div>
         </article>
     );
